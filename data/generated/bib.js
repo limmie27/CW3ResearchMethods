75ab406d90c45e0de9a358c678e5c78a53e925ad
limmie27 <151518794+limmie27@users.noreply.github.com>
2024-05-17 11:50:12 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,115 @@
+﻿define({ entries : {
+    "Abdulmalek2022": {
+        "author": "Abdulmalek, S. and Nasir, A. and Jabbar, W. and Almuhaya, A. M. and Bairagi, A. K. and Khan, A.-M. and Kee, S.-H.",
+        "doi": "10.3390/healthcare10101993",
+        "journal": "Healthcare",
+        "keywords": "IoT, Healthcare, Quality of Life",
+        "pages": "1--32",
+        "title": "IoT-Based Healthcare-Monitoring System towards Improving Quality of Life: A Review",
+        "type": "article",
+        "volume": "10",
+        "year": "2022"
+    },
+    "Ahmad2022": {
+        "author": "Ahmad, I. and Asghar, Z. and Kumar, T. and Li, G. and Manzoor, A. and Mikhaylov, K. and Shah, S. A. and Hoyhtya, M. and Reponen, J. and Huusko, J. and Harjula, E.",
+        "doi": "10.1109/ACCESS.2022.3177278",
+        "journal": "IEEE Access",
+        "keywords": "Emerging Technologies, Remote Healthcare, Assisted Living",
+        "pages": "56094--56132",
+        "title": "Emerging Technologies for Next Generation Remote Health Care and Assisted Living",
+        "type": "article",
+        "volume": "10",
+        "year": "2022"
+    },
+    "Alshamnrani2022": {
+        "author": "Alshamnrani, M.",
+        "doi": "10.1016/j.jksuci.2021.06.005",
+        "journal": "Journal of King Saud University - Computer and Information Sciences",
+        "keywords": "IoT, Artificial Intelligence, Remote Healthcare Monitoring",
+        "pages": "4687--4701",
+        "title": "IoT and Artificial Intelligence Implementations for Remote Healthcare Monitoring Systems: A Survey",
+        "type": "article",
+        "volume": "34",
+        "year": "2022"
+    },
+    "Greco2020": {
+        "author": "Greco, L. and Percannella, G. and Ritrovato, P. and Tortorella, F. and Vento, M.",
+        "doi": "10.1016/j.patrec.2020.05.016",
+        "journal": "Pattern Recognition Letters",
+        "keywords": "IoT, AI, Edge Computing, Healthcare",
+        "pages": "346--353",
+        "title": "Trends in IoT Based Solutions for Health Care: Moving AI to the Edge",
+        "type": "article",
+        "volume": "135",
+        "year": "2020"
+    },
+    "Jeong2021": {
+        "author": "Jeong, S. and Shen, J.-H. and Ahn, B. and Sangaiah, A. K.",
+        "doi": "10.1155/2021/9932091",
+        "journal": "Wireless Communications and Mobile Computing",
+        "keywords": "Smart Healthcare, IoT, Blockchain",
+        "pages": "1--9",
+        "title": "A Study on Smart Healthcare Monitoring Using IoT Based on Blockchain",
+        "type": "article",
+        "volume": "2021",
+        "year": "2021"
+    },
+    "Kang2018": {
+        "author": "Kang, M. and Park, E. and Cho, B. H. and Lee, K.-S.",
+        "doi": "10.5213/inj.1836144.072",
+        "journal": "International Neurourology Journal",
+        "keywords": "Patient Monitoring, IoT, Smart Devices",
+        "pages": "76--82",
+        "title": "Recent Patient Health Monitoring Platforms Incorporating Internet of Things-Enabled Smart Devices",
+        "type": "article",
+        "volume": "22",
+        "year": "2018"
+    },
+    "Lakshmi2021": {
+        "author": "Lakshmi, G. and Ghonge, M. and Obaid, A.",
+        "doi": "10.4108/eai.15-7-2021.170296",
+        "journal": "EAI Endorsed Transactions on Pervasive Health and Technology",
+        "keywords": "Cloud Computing, IoT, Smart Healthcare, Remote Monitoring",
+        "number": "28",
+        "pages": "1--11",
+        "title": "Cloud Based IoT Smart Healthcare System for Remote Patient Monitoring",
+        "type": "article",
+        "volume": "7",
+        "year": "2021"
+    },
+    "Muneer2020": {
+        "author": "Muneer, A. and Fati, S. M. and Fuddah, S.",
+        "doi": "10.12928/telkomnika.v18i1.12434",
+        "journal": "TELKOMNIKA",
+        "keywords": "Smart Health, IoT, Fitness Monitoring",
+        "number": "1",
+        "pages": "317--331",
+        "title": "Smart Health Monitoring System Using IoT Based Smart Fitness Mirror",
+        "type": "article",
+        "volume": "18",
+        "year": "2020"
+    },
+    "Rathi2021": {
+        "author": "Rathi, V. K. and Rajput, N. K. and Mishra, S. and Grover, B. A. and Tiwari, P. and Jaiswal, A. K. and Hossain, M. S.",
+        "doi": "10.1016/j.compeleceng.2021.107524",
+        "journal": "Computers & Electrical Engineering",
+        "keywords": "Edge AI, IoT, Healthcare Monitoring, Smart Cities",
+        "pages": "1--12",
+        "title": "An Edge AI-Enabled IoT Healthcare Monitoring System for Smart Cities",
+        "type": "article",
+        "volume": "96",
+        "year": "2021"
+    },
+    "Shaik2022": {
+        "author": "Shaik, T. and Tao, X. and Higgins, N. and Li, L. and Gururajan, R. and Zhou, X. and Acharya, U. R.",
+        "doi": "10.1002/widm.1485",
+        "journal": "Wiley Interdisciplinary Reviews: Data Mining and Knowledge Discovery",
+        "keywords": "Remote Monitoring, Artificial Intelligence, Healthcare",
+        "number": "2",
+        "pages": "1--31",
+        "title": "Remote Patient Monitoring Using Artificial Intelligence: Current State, Applications, and Challenges",
+        "type": "article",
+        "volume": "13",
+        "year": "2022"
+    }
+}});
